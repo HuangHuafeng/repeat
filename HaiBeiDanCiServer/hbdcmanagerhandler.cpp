67a2e21fa8038fd30089791d11f810403d147a77
HuafengHuang <huafeng.huang@gmail.com>
2018-09-02 21:13:06 +0800

    should be OK.

---
+++ HaiBeiDanCiServer/hbdcmanagerhandler.cpp
@@ -0,0 +1,6 @@
+#include "hbdcmanagerhandler.h"
+
+HBDCManagerHandler::HBDCManagerHandler()
+{
+
+}
---
+++ HaiBeiDanCiServer/hbdcmanagerhandler.h
@@ -0,0 +1,11 @@
+#ifndef HBDCMANAGERHANDLER_H
+#define HBDCMANAGERHANDLER_H
+
+
+class HBDCManagerHandler : public HBDCAppHandler
+{
+public:
+    HBDCManagerHandler();
+};
+
+#endif // HBDCMANAGERHANDLER_H
